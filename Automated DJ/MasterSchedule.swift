2ec6002e85e3b698ca99aa60074b8cda6032a886
Zach <zachary.whitten.11@cnu.edu>
2016-05-12 16:54:59 -0400

    Linked the dataSource to the tableView

--- Automated DJ/MasterSchedule.swift
+++ Automated DJ/MasterSchedule.swift
@@ -22,35 +22,52 @@ class MasterSchedule: NSObject, NSTableViewDataSource, NSTableViewDelegate {
     
     func addShow(aShow: Show){
         dataArray.append(aShow)
+        print(dataArray)
         tableView.reloadData()
     }
     
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
-        return getDataArray().count
+        return dataArray.count
     }
     
     
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        // get the item for the row
-        let items = getDataArray()
-        let item = items[row]
-        
-        var cellView: NSTableCellView?
-       
-        // get the NSTableCellView for the column
+        //Get the column identifer, the show for the current row, and create an empty cellView variable
         let identifier = tableColumn?.identifier
-
+        let aShow = dataArray[row]
+        var cellView: NSTableCellView?
+        
+        //What column we are working with determins what show information is placed into it. Inside the if statenment places information into a single cell view
         if identifier == "showName" {
+            //Create cellView and set its value to the show name. ("ShowNameCell" needs to be defined in IB and be an actual cell in the default table view)
             cellView = (tableView.makeViewWithIdentifier("ShowNameCell", owner: nil) as? NSTableCellView)!
-            cellView!.textField?.stringValue = item.valueForKey(identifier!) as! String
+            cellView!.textField?.stringValue = aShow.name!
         }
         else if identifier == "time"{
+            //Create cell view
             cellView = (tableView.makeViewWithIdentifier("TimeCell", owner: nil) as? NSTableCellView)!
-            cellView!.textField?.stringValue = item.valueForKey(identifier!) as! String
+            //Create date formatters for time and day
+            let timeFormatter = NSDateFormatter()
+            timeFormatter.dateFormat = "hh:mm a"
+            let dayFormatter = NSDateFormatter()
+            dayFormatter.dateFormat = "EEEE"
+            //Convert the shows dates into strings
+            let startDay = dayFormatter.stringFromDate(aShow.startDate!)
+            let startTime = timeFormatter.stringFromDate(aShow.startDate!)
+            let endDay = dayFormatter.stringFromDate(aShow.endDate!)
+            let endTime = timeFormatter.stringFromDate(aShow.endDate!)
+            //Append those strings together and set their value to the cell view
+            cellView!.textField?.stringValue = startDay + ", " + startTime + " to " + endDay + ", " + endTime
         }
         else if identifier == "automated"{
+            //Create cell view and if there's not an automator set its value to no otherwise set it to no
             cellView = (tableView.makeViewWithIdentifier("AutomatedCell", owner: nil) as? NSTableCellView)!
-            cellView!.textField?.stringValue = item.valueForKey(identifier!) as! String
+            if aShow.automator == nil {
+                 cellView!.textField?.stringValue = "No"
+            }
+            else{
+                 cellView!.textField?.stringValue = "Yes"
+            }
         }
         
         // return the populated NSTableCellView
