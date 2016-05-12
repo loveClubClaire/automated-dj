3b06b91604baddcc1c6a42362a404d1c6b2d358e
Zach <zachary.whitten.11@cnu.edu>
2016-05-12 14:29:10 -0400

    Added ShowWindow cancel button functionality

--- Automated DJ/ShowWindow.swift
+++ Automated DJ/ShowWindow.swift
@@ -15,6 +15,8 @@ class ShowWindow: NSObject {
     @IBOutlet weak var startTime: NSDatePicker!
     @IBOutlet weak var endTime: NSDatePicker!
     @IBOutlet weak var isAutomated: NSButton!
+    @IBOutlet weak var startDay: NSPopUpButton!
+    @IBOutlet weak var endDay: NSPopUpButton!
     
 
     func spawnNewShowWindow(){
@@ -34,10 +36,30 @@ class ShowWindow: NSObject {
     }
     
     @IBAction func okButton(sender: AnyObject) {
+        
     }
     
     
     @IBAction func cancelButton(sender: AnyObject) {
+        //Create the NSDates which contain the default values for the two NSDatePickers
+        let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)
+        let startTimeComponents = NSDateComponents()
+        startTimeComponents.hour = 8
+        let endTimeComponents = NSDateComponents()
+        endTimeComponents.hour = 10
+        //Set the NSDatePickers to their default values
+        startTime.dateValue = (calendar?.dateFromComponents(startTimeComponents))!
+        endTime.dateValue = (calendar?.dateFromComponents(endTimeComponents))!
+        //Set the show name to its default value
+        showName.stringValue = ""
+        //Set the NSPopUpButtons to their default values (Index at 0 should be Monday)
+        startDay.selectItemAtIndex(0)
+        endDay.selectItemAtIndex(0)
+        //Set the isAutomated Button to its default value (off)
+        isAutomated.state = 0
+        //Stop the NSApp modal and dismiss the window
+        NSApp.stopModal()
+        showWindow.orderOut(self)
     }
  
     
