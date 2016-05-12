d2bb32658a3607677afbbaab390925bd6d0bb8e5
Zach <zachary.whitten.11@cnu.edu>
2016-05-12 13:58:24 -0400

    Created showWindow skeleton

--- Automated DJ/ShowWindow.swift
+++ Automated DJ/ShowWindow.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Cocoa
+
+class ShowWindow: NSObject {
+    @IBOutlet weak var showWindow: NSWindow!
+    @IBOutlet weak var showName: NSTextField!
+    @IBOutlet weak var startTime: NSDatePicker!
+    @IBOutlet weak var endTime: NSDatePicker!
+    @IBOutlet weak var isAutomated: NSButton!
+    
+
+    func spawnShowWindow(){
+        
+    }
+    
+    @IBAction func okButton(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func cancelButton(sender: AnyObject) {
+    }
+ 
+    
+}
