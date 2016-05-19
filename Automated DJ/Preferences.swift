0c16d063fe3128f8f34477b4a538dda8138ae1f4
Zach <zachary.whitten.11@cnu.edu>
2016-05-19 19:10:59 -0400

    Added ok and cancel functionality to preferences

--- Automated DJ/Preferences.swift
+++ Automated DJ/Preferences.swift
@@ -16,20 +16,42 @@ class Preferences: NSObject {
     @IBOutlet weak var globalAnnouncementsDelayTextField: NSTextField!
     @IBOutlet weak var isAdminButton: NSButton!
     @IBOutlet weak var tollerenceTextField: NSTextField!
-    @IBOutlet weak var testAdminButton: NSButton!
+    @IBOutlet weak var testAutomatorButton: NSButton!
 
+    private var tempAutomator: Automator = Automator()
     var defaultAutomator: Automator = Automator()
+    var isAdmin = false
+    var globalAnnouncementsDelay = 0
+    var tollerence = 0
+    var testAutomator = true
+    
+    func spawnPreferencesWindow(){
+        tempAutomator = defaultAutomator
+        preferencesWindow.center()
+        preferencesWindow.makeKeyAndOrderFront(self)
+    }
     
     @IBAction func customizeDefaultAutomator(sender: AnyObject) {
-        
+        //Sets tempAutomator
     }
     
     @IBAction func okButton(sender: AnyObject) {
-        
+        defaultAutomator = tempAutomator
+        if isAdminButton.state == NSOnState {isAdmin = true}
+        else{isAdmin = false}
+        if testAutomatorButton.state == NSOnState {testAutomator = true}
+        else{testAutomator = false}
+        globalAnnouncementsDelay = globalAnnouncementsDelayTextField.integerValue
+        tollerence = tollerenceTextField.integerValue
     }
     
     @IBAction func cancelButton(sender: AnyObject) {
-        
+        if isAdmin == true {isAdminButton.state = NSOnState}
+        else{isAdminButton.state = NSOffState}
+        if testAutomator == true {testAutomatorButton.state = NSOnState}
+        else{testAutomatorButton.state = NSOffState}
+        globalAnnouncementsDelayTextField.integerValue = globalAnnouncementsDelay
+        tollerenceTextField.integerValue = tollerence
     }
 
     
