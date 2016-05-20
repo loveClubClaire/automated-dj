0888024a48cc4c8be8929b9db528428cf7dc580c
Zach <zachary.whitten.11@cnu.edu>
2016-05-19 20:20:52 -0400

    Added skeleton encoder and decoder

--- Automated DJ/Automator.swift
+++ Automated DJ/Automator.swift
@@ -12,14 +12,24 @@ class Automator: NSObject {
     
     
     
+  
+
+    //Decode each individual object and then create a new object instance
+    required convenience init?(coder decoder: NSCoder) {
+        self.init()
+    }
+    //Encoding fucntion for saving. Encode each object with a key for retervial
+    func encodeWithCoder(coder: NSCoder) {
+       
+    }
     
-func compare(anAutomator: Automator?) -> NSComparisonResult {
-    if anAutomator == nil {
-        return NSComparisonResult.OrderedDescending
-    }
-    else{
-        return NSComparisonResult.OrderedSame
-    }
+    func compare(anAutomator: Automator?) -> NSComparisonResult {
+        if anAutomator == nil {
+            return NSComparisonResult.OrderedDescending
+        }
+        else{
+            return NSComparisonResult.OrderedSame
+        }
 }
     
 }
