e539a0cbb80c2f6a6333c0016c727e9a27d71f5c
Zach <zachary.whitten.11@cnu.edu>
2016-11-15 22:20:52 -0500

    Converted GloablAnnouncements to Swift 3

--- Automated DJ/GlobalAnnouncements.swift
+++ Automated DJ/GlobalAnnouncements.swift
@@ -25,63 +25,63 @@ class GloablAnnouncements: NSObject, NSTableViewDataSource, NSTableViewDelegate 
     var dataArray = [String]()
     
     func spawnMutableGlobalAnnouncements(){
-        addAnnouncementButton.view?.hidden = false
-        editMenuItem.hidden = false; deleteMenuItem.hidden = false;
+        addAnnouncementButton.view?.isHidden = false
+        editMenuItem.isHidden = false; deleteMenuItem.isHidden = false;
         globalAnnouncementsWindow.center()
         globalAnnouncementsWindow.makeKeyAndOrderFront(self)
         isMutable = true
     }
     
     func spawnImmutableGlobalAnnouncements(){
-        addAnnouncementButton.view?.hidden = true
-        editMenuItem.hidden = true; deleteMenuItem.hidden = true;
+        addAnnouncementButton.view?.isHidden = true
+        editMenuItem.isHidden = true; deleteMenuItem.isHidden = true;
         globalAnnouncementsWindow.center()
         globalAnnouncementsWindow.makeKeyAndOrderFront(self)
         isMutable = false
     }
     
-    @IBAction func spawnNewAnnouncementWindow(sender: AnyObject) {
+    @IBAction func spawnNewAnnouncementWindow(_ sender: AnyObject) {
         addGlobalAnnouncementWindow.title = "New Announcement"
         addGlobalAnnouncementWindow.center()
         addGlobalAnnouncementWindow.makeKeyAndOrderFront(self)
-        NSApp.runModalForWindow(addGlobalAnnouncementWindow)
+        NSApp.runModal(for: addGlobalAnnouncementWindow)
     }
     
-    @IBAction func addAnnouncement(sender: AnyObject) {
+    @IBAction func addAnnouncement(_ sender: AnyObject) {
         if addGlobalAnnouncementWindow.title == "New Announcement" {
             dataArray.append(addGlobalAnnouncementText.string!)
         }
         else{
-            dataArray.removeAtIndex(tableView.selectedRow)
-            dataArray.insert((addGlobalAnnouncementText.string!), atIndex: tableView.selectedRow)
+            dataArray.remove(at: tableView.selectedRow)
+            dataArray.insert((addGlobalAnnouncementText.string!), at: tableView.selectedRow)
         }
         NSKeyedArchiver.archiveRootObject(dataArray, toFile: AppDelegateObject.storedAnnouncementsFilepath)
         tableView.reloadData()
         cancelNewAnnouncement(self)
     }
     
-    @IBAction func cancelNewAnnouncement(sender: AnyObject) {
+    @IBAction func cancelNewAnnouncement(_ sender: AnyObject) {
         addGlobalAnnouncementWindow.title = "New Announcement"
         addGlobalAnnouncementText.string = ""
         NSApp.stopModal()
         addGlobalAnnouncementWindow.orderOut(self)
     }
     
-    @IBAction func editAnnouncement(sender: AnyObject) {
+    @IBAction func editAnnouncement(_ sender: AnyObject) {
         //get all selected elements in the tableview
         var selectedShows = tableView.selectedRowIndexes
         //If the clicked show (if there is one) is not contained in the NSIndexSet, then add it. Because NSIndexSet is not mutable, we need to convert it into a NSMutableIndexSet, add the new value, and then set selectedShows to that new mutable index set.
-        if(selectedShows.containsIndex(tableView.clickedRow) == false && tableView.clickedRow != -1){
+        if(selectedShows.contains(tableView.clickedRow) == false && tableView.clickedRow != -1){
             let selectedShowsMutable = NSMutableIndexSet.init(indexSet: selectedShows)
-            selectedShowsMutable.addIndex(tableView.clickedRow)
-            selectedShows = selectedShowsMutable
+            selectedShowsMutable.add(tableView.clickedRow)
+            selectedShows = selectedShowsMutable as IndexSet
         }
         //User can not edit more than one announcement at a time, so this precents this.
         if selectedShows.count > 1 {
             let myPopup: NSAlert = NSAlert()
             myPopup.messageText = "Can not edit multiple announcements"
-            myPopup.alertStyle = NSAlertStyle.CriticalAlertStyle
-            myPopup.addButtonWithTitle("OK")
+            myPopup.alertStyle = NSAlertStyle.critical
+            myPopup.addButton(withTitle: "OK")
             myPopup.runModal()
         }
         else if selectedShows.count == 1{
@@ -89,32 +89,32 @@ class GloablAnnouncements: NSObject, NSTableViewDataSource, NSTableViewDelegate 
             addGlobalAnnouncementText.string = dataArray[tableView.selectedRow]
             addGlobalAnnouncementWindow.center()
             addGlobalAnnouncementWindow.makeKeyAndOrderFront(self)
-            NSApp.runModalForWindow(addGlobalAnnouncementWindow)
+            NSApp.runModal(for: addGlobalAnnouncementWindow)
         }
         
     }
     
-    @IBAction func deleteAnnouncements(sender: AnyObject) {
+    @IBAction func deleteAnnouncements(_ sender: AnyObject) {
         //get all selected elements in the tableview
         var selectedShows = tableView.selectedRowIndexes
         //If the clicked show (if there is one) is not contained in the NSIndexSet, then add it. Because NSIndexSet is not mutable, we need to convert it into a NSMutableIndexSet, add the new value, and then set selectedShows to that new mutable index set.
-        if(selectedShows.containsIndex(tableView.clickedRow) == false && tableView.clickedRow != -1){
+        if(selectedShows.contains(tableView.clickedRow) == false && tableView.clickedRow != -1){
             let selectedShowsMutable = NSMutableIndexSet.init(indexSet: selectedShows)
-            selectedShowsMutable.addIndex(tableView.clickedRow)
-            selectedShows = selectedShowsMutable
+            selectedShowsMutable.add(tableView.clickedRow)
+            selectedShows = selectedShowsMutable as IndexSet
         }
         //If the number of shows selected is greater than zero, alert the user about the deletion. If they agree to it, remove the items from the dataArray, save the new state of the dataArray, and refresh the tableView
         if selectedShows.count > 0 {
             let myPopup: NSAlert = NSAlert()
             myPopup.messageText = "Are you sure you want to delete the selected announcements?"
             myPopup.informativeText = "This can not be undone"
-            myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
-            myPopup.addButtonWithTitle("OK")
-            myPopup.addButtonWithTitle("Cancel")
+            myPopup.alertStyle = NSAlertStyle.warning
+            myPopup.addButton(withTitle: "OK")
+            myPopup.addButton(withTitle: "Cancel")
             let res = myPopup.runModal()
             if res == NSAlertFirstButtonReturn {
                 let dataMutableArray = NSMutableArray(array: dataArray)
-                dataMutableArray.removeObjectsAtIndexes(selectedShows)
+                dataMutableArray.removeObjects(at: selectedShows)
                 dataArray = dataMutableArray as AnyObject as! [String]
                 NSKeyedArchiver.archiveRootObject(dataArray, toFile: AppDelegateObject.storedAnnouncementsFilepath)
                 tableView.reloadData()
@@ -122,13 +122,13 @@ class GloablAnnouncements: NSObject, NSTableViewDataSource, NSTableViewDelegate 
     }
     }
 
-    func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
         //set the TextView (Which as the same width as the GlobalAnnouncements tableView cell) to the string at the given row in the data array. Then calculate how large the cell needs to be and retrun that value.
         settingTextView.string = dataArray[row]
         return (17.0 * CGFloat(numOfLinesIn(settingTextView)))
     }
     
-    func numOfLinesIn(aTextView: NSTextView) -> Int {
+    func numOfLinesIn(_ aTextView: NSTextView) -> Int {
         let layoutManager = aTextView.layoutManager
         var numberOfLines = 0
         var index = 0
@@ -136,7 +136,7 @@ class GloablAnnouncements: NSObject, NSTableViewDataSource, NSTableViewDelegate 
         let numberOfGlyphs = layoutManager!.numberOfGlyphs
         
         while index < numberOfGlyphs {
-            layoutManager!.lineFragmentRectForGlyphAtIndex(index, effectiveRange: &lineRange)
+            layoutManager!.lineFragmentRect(forGlyphAt: index, effectiveRange: &lineRange)
             index = NSMaxRange(lineRange);
             numberOfLines += 1
         }
@@ -145,14 +145,14 @@ class GloablAnnouncements: NSObject, NSTableViewDataSource, NSTableViewDelegate 
         return numberOfLines
     }
     
-    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+    func numberOfRows(in tableView: NSTableView) -> Int {
         return dataArray.count
     }
     
-    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         //Get the announcement for the current row, and create an cellView variable
         let anAnnouncement = dataArray[row]
-        let cellView = (tableView.makeViewWithIdentifier("announcementsCell", owner: nil) as? NSTableCellView)!
+        let cellView = (tableView.make(withIdentifier: "announcementsCell", owner: nil) as? NSTableCellView)!
         //set the string value for the current row to its corresponding announcement
         cellView.textField?.stringValue = anAnnouncement
         // return the populated NSTableCellView
