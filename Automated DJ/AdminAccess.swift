91e04f80e3ab2fad3e7a413074df9d0c17621081
Zach <zachary.whitten.11@cnu.edu>
2016-11-15 21:18:11 -0500

    Updated AdminAccess class to Swift 3

--- Automated DJ/AdminAccess.swift
+++ Automated DJ/AdminAccess.swift
@@ -11,16 +11,16 @@ import Cocoa
 
 class AdminAccess: NSObject {
     @IBOutlet weak var PreferencesObject: Preferences!
-    private func getAdminAccess() -> Bool {
+    fileprivate func getAdminAccess() -> Bool {
         var result = false;
-        var authRef: AuthorizationRef = nil
-        var myStatus = AuthorizationCreate(nil, nil, AuthorizationFlags.Defaults, &authRef)
+        var authRef: AuthorizationRef? = nil
+        var myStatus = AuthorizationCreate(nil, nil, AuthorizationFlags(), &authRef)
         var right = AuthorizationItem.init(name: kAuthorizationRightExecute, valueLength: 0, value: nil, flags: 0)
         var rights = AuthorizationRights.init(count: 1, items: &right)
         //The AuthorizationFlags.DestroyRights flag is what forces the system to ask for an admin password EACH time this function is called. Prevents a sucessful authorization from being remembered
-        let flags = AuthorizationFlags(rawValue: AuthorizationFlags.Defaults.rawValue |  AuthorizationFlags.InteractionAllowed.rawValue | AuthorizationFlags.ExtendRights.rawValue | AuthorizationFlags.DestroyRights.rawValue)
+        let flags = AuthorizationFlags(rawValue: AuthorizationFlags().rawValue |  AuthorizationFlags.interactionAllowed.rawValue | AuthorizationFlags.extendRights.rawValue | AuthorizationFlags.destroyRights.rawValue)
         //Call AuthorizationCopyRights to determine or extend the allowable rights.
-        myStatus = AuthorizationCopyRights(authRef, &rights, nil, flags, nil)
+        myStatus = AuthorizationCopyRights(authRef!, &rights, nil, flags, nil)
         if myStatus == errAuthorizationSuccess {
             result = true
         }
