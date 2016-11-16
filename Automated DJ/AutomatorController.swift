19e7f97e11fba3386035984de5cc06237a166a28
Zach <zachary.whitten.11@cnu.edu>
2016-11-15 22:17:50 -0500

    Converted AutomatorController to Swift 3

--- Automated DJ/AutomatorController.swift
+++ Automated DJ/AutomatorController.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 import Cocoa
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
 
 class AutomatorController: NSObject {
     @IBOutlet weak var MasterScheduleObject: MasterSchedule!
@@ -16,24 +27,24 @@ class AutomatorController: NSObject {
     @IBOutlet weak var AppDelegateObject: AppDelegate!
 
     func spawnMasterTimer(){
-        NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: #selector(masterTimerFunction), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(masterTimerFunction), userInfo: nil, repeats: true)
     }
     
     func masterTimerFunction(){
         let applescriptBridge = ApplescriptBridge()
         //Create a date object 3 minutes in the future (NOTE: Because of how the timer is configured - to fire every minute - this we will give us a max of three minutes before a show and a minimum of two)
-        let futureTime = NSDate.init().dateByAddingTimeInterval(180)
+        let futureTime = Date.init().addingTimeInterval(180)
         //Create date formatter, which represents a date as a weekday and the hour and minute. And two more for minute and second respectivally.
-        let dateFormatterShowTime = NSDateFormatter()
+        let dateFormatterShowTime = DateFormatter()
         dateFormatterShowTime.dateFormat = "EEEE 'at' HH:mm"
-        let minuteFormatter = NSDateFormatter()
+        let minuteFormatter = DateFormatter()
         minuteFormatter.dateFormat = "mm"
-        let secondFormatter = NSDateFormatter()
+        let secondFormatter = DateFormatter()
         secondFormatter.dateFormat = "ss"
         
         //Iterate though all shows and if the start time of a show matches the future time... (Do things)
         for aShow in MasterScheduleObject.dataArray {
-            if dateFormatterShowTime.stringFromDate(aShow.startDate!) == dateFormatterShowTime.stringFromDate(futureTime){
+            if dateFormatterShowTime.string(from: aShow.startDate! as Date) == dateFormatterShowTime.string(from: futureTime){
                 //Determine the name of our generated playlist. This is done to prevent name overlap. We also delete any automated DJ playlist which is not currently playing so that we do not have duplicates of the same playlist.
                 var generatedPlaylistName = "Automated DJ"
                 //get currently playing playlist
@@ -50,21 +61,21 @@ class AutomatorController: NSObject {
                     generatedPlaylistName = "Automated DJ2"
                 }
                 //Get the number of seconds into the hour that the show starts and of the current time. Get the difference of those two numbers and we have the number of seconds until the show begins, which we use as our delay time. If the show starts in the next hour relative to the hour of the current time, then the resulting difference is a negative number. Adding 3600 to that negative number gets us the positive number of seconds until the show begins.
-                let showStartTimeSeconds = (Int(minuteFormatter.stringFromDate(aShow.startDate!))! * 60)
-                let currentTimeSeconds = (Int(minuteFormatter.stringFromDate(NSDate.init()))! * 60) + (Int(secondFormatter.stringFromDate(NSDate.init())))!
+                let showStartTimeSeconds = (Int(minuteFormatter.string(from: aShow.startDate! as Date))! * 60)
+                let currentTimeSeconds = (Int(minuteFormatter.string(from: Date.init()))! * 60) + (Int(secondFormatter.string(from: Date.init())))!
                 var delay = showStartTimeSeconds - currentTimeSeconds
                 if delay < 0 {delay = delay + 3600}
                 //If the show is not automated then only the global announcements window needs to be spawned
                 if aShow.automator == nil {
-                    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(delay + PreferencesObject.globalAnnouncementsDelay) * Double(NSEC_PER_SEC)))
-                    dispatch_after(delayTime, dispatch_get_main_queue()) {
+                    let delayTime = DispatchTime.now() + Double(Int64(Double(delay + PreferencesObject.globalAnnouncementsDelay) * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                    DispatchQueue.main.asyncAfter(deadline: delayTime) {
                         self.GlobalAnnouncementsObject.spawnImmutableGlobalAnnouncements()
                     }
                 }
                 //If the show is automated, a playlist must be generated and played
                 else{
-                    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(delay) * Double(NSEC_PER_SEC)))
-                    dispatch_after(delayTime, dispatch_get_main_queue()) {
+                    let delayTime = DispatchTime.now() + Double(Int64(Double(delay) * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                    DispatchQueue.main.asyncAfter(deadline: delayTime) {
                        self.playGeneratedPlaylist(generatedPlaylistName)
                     }
                     generatePlaylist(generatedPlaylistName, anAutomator: aShow.automator!)
@@ -74,29 +85,29 @@ class AutomatorController: NSObject {
         }
     }
     
-    func generatePlaylist(playlistName: String, anAutomator: Automator){
-        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+    func generatePlaylist(_ playlistName: String, anAutomator: Automator){
+        let priority = DispatchQueue.GlobalQueuePriority.default
+        DispatchQueue.global(priority: priority).async {
         //Initalize logging variables
-        let startTime = NSDate.init(); var tier1log = 0; var tier2log = 0; var tier3log = 0;
+        let startTime = Date.init(); var tier1log = 0; var tier2log = 0; var tier3log = 0;
         //NOTE: Automator.totalTime is an hour value! Its a double representing hours. So it needs to be converted to seconds if you want seconds
         let applescriptBridge = ApplescriptBridge()
         let generatedPlaylist: NSMutableArray = NSMutableArray()
         //Get tiered playlists
         self.AppDelegateObject.updateCachedPlaylists()
-        let tier1 = NSMutableArray(); tier1.addObjectsFromArray(self.AppDelegateObject.cachedTier1Playlist as [AnyObject])
-        let tier2 = NSMutableArray(); tier2.addObjectsFromArray(self.AppDelegateObject.cachedTier2Playlist as [AnyObject])
-        let tier3 = NSMutableArray(); tier3.addObjectsFromArray(self.AppDelegateObject.cachedTier3Playlist as [AnyObject])
+        let tier1 = NSMutableArray(); tier1.addObjects(from: self.AppDelegateObject.cachedTier1Playlist as [AnyObject])
+        let tier2 = NSMutableArray(); tier2.addObjects(from: self.AppDelegateObject.cachedTier2Playlist as [AnyObject])
+        let tier3 = NSMutableArray(); tier3.addObjects(from: self.AppDelegateObject.cachedTier3Playlist as [AnyObject])
         //Apply rules to tiered playlists if rules exist
         if anAutomator.rules != nil {
-            tier1.filterUsingPredicate(anAutomator.rules!)
-            tier2.filterUsingPredicate(anAutomator.rules!)
-            tier3.filterUsingPredicate(anAutomator.rules!)
+            tier1.filter(using: anAutomator.rules!)
+            tier2.filter(using: anAutomator.rules!)
+            tier3.filter(using: anAutomator.rules!)
         }
         //get seed playlist if it exists and add seed playlist to generated playlist
         //TODO test if this handles a non existant but non null playlist
         if anAutomator.seedPlayist != nil{
-            generatedPlaylist.addObjectsFromArray(applescriptBridge.getSongsInPlaylist(anAutomator.seedPlayist!) as [AnyObject])
+            generatedPlaylist.addObjects(from: applescriptBridge.getSongsInPlaylist(anAutomator.seedPlayist!) as [AnyObject])
             //Remove excess if seed playlist is longer than playlist length
             //While actual time is greater than expected time + tolerance
             while Playlist.getNewPlaylistDuration(generatedPlaylist as Array as! [Song]) > ((anAutomator.totalTime * 3600) + Double(self.PreferencesObject.tollerence)) {
@@ -120,7 +131,7 @@ class AutomatorController: NSObject {
             //bumperBlockCount keeps track of how large our current block is, bumperCounter keeps track of where we are in the bumper playlist. This number rolls over so that once we reach the end of the bumper playlist, we go back to the begining rather than just ending or something. And songBlockCount keeps track of the number of songs added since we last added a bumper
             if songBlockCount == songsBetweenBlocks && anAutomator.bumpersPlaylist != nil {
                 while bumperBlockCount < anAutomator.bumpersPerBlock {
-                    generatedPlaylist.addObject(bumpers.objectAtIndex(bumperCounter))
+                    generatedPlaylist.add(bumpers.object(at: bumperCounter))
                     bumperBlockCount = bumperBlockCount + 1; bumperCounter = bumperCounter + 1
                     if bumperCounter >= bumpers.count {
                         bumperCounter = 0
@@ -153,9 +164,9 @@ class AutomatorController: NSObject {
                 
                 while tieredPlaylist.count > 0 {
                     let songPosition = Int(arc4random_uniform(UInt32(tieredPlaylist.count)))
-                    let randomSong = tieredPlaylist.objectAtIndex(songPosition) as! Song; tieredPlaylist.removeObjectAtIndex(songPosition)
+                    let randomSong = tieredPlaylist.object(at: songPosition) as! Song; tieredPlaylist.removeObject(at: songPosition)
                     if (Playlist.getNewPlaylistDuration(generatedPlaylist as Array as! [Song]) + randomSong.duration) < ((anAutomator.totalTime * 3600) + Double(self.PreferencesObject.tollerence)) {
-                            generatedPlaylist.addObject(randomSong)
+                            generatedPlaylist.add(randomSong)
                             songBlockCount = songBlockCount + 1
                             break;
                     }
@@ -170,7 +181,7 @@ class AutomatorController: NSObject {
         }
         //Once we generated our playlist, create the playlist in iTunes and add all the songs to the iTunes playlist.
         applescriptBridge.createPlaylistWithName(playlistName)
-        applescriptBridge.addSongsToPlaylist(playlistName, songArray: generatedPlaylist as [AnyObject])
+        applescriptBridge.addSongsToPlaylist(playlistName, songArray: generatedPlaylist)
         //Logging
         let log = LogGenerator.init()
         log.writeToLog("Playlist generation time (in seconds): " + String(startTime.timeIntervalSinceNow * -1.0))
@@ -184,7 +195,7 @@ class AutomatorController: NSObject {
         }
     }
     
-    func playGeneratedPlaylist(playlistName: String){
+    func playGeneratedPlaylist(_ playlistName: String){
         let applescriptBridge = ApplescriptBridge()
         //Disable shuffle && looping
         applescriptBridge.disableShuffle()
@@ -192,16 +203,17 @@ class AutomatorController: NSObject {
         //delay until current song concludes
         //TODO test how good the delay is. Subtracting half a second from the dealy is a holdover from the 1.0 release. Make sure its still necessary
         var rawDelayTime = applescriptBridge.timeLeftInCurrentSong()
-        var delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64( (rawDelayTime - 0.5) * Double(NSEC_PER_SEC)))
+        var delayTime = DispatchTime.now() + Double(Int64( (rawDelayTime - 0.5) * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
         //If iTunes is not playing, then set the delay to 0. (This needs to be done because if a song is paused midway, then timeLeftInCurrentSong will return a greater than zero value, because thats how much time is left in the song! THe only issue is that the song isnt playing. So unchecked, we would wait in silence)
         if applescriptBridge.isiTunesPlaying() == false {
-            delayTime = 0
+            //In theory, this sets delay time to zero.
+            delayTime = DispatchTime.init(uptimeNanoseconds: 0)
             rawDelayTime = 0
         }
-        dispatch_after(delayTime, dispatch_get_main_queue()) {
+        DispatchQueue.main.asyncAfter(deadline: delayTime) {
             applescriptBridge.playPlaylist(playlistName)
             let log = LogGenerator()
-            log.writeToLog("Start time of playlist: " + NSDate.init().description)
+            log.writeToLog("Start time of playlist: " + Date.init().description)
             log.writeToLog("------------------------------------------------")
         }
         //trim excess fat from generated playlist if possible.
