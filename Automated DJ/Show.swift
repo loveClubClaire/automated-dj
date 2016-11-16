8d42664c413c33e5ee7f3a13ad0ffe6fb7ce9145
Zach <zachary.whitten.11@cnu.edu>
2016-11-15 21:52:12 -0500

    Converted Show to Swift 3

--- Automated DJ/Show.swift
+++ Automated DJ/Show.swift
@@ -12,11 +12,11 @@ import Automator
 class Show: NSObject, NSCoding{
     //Initial defination of object variables
     var name: String?
-    var startDate: NSDate?
-    var endDate: NSDate?
+    var startDate: Date?
+    var endDate: Date?
     var automator: Automator?
     
-    init(aName: String, aStartDate: NSDate, anEndDate: NSDate) {
+    init(aName: String, aStartDate: Date, anEndDate: Date) {
         name = aName
         startDate = aStartDate
         endDate = anEndDate
@@ -24,27 +24,27 @@ class Show: NSObject, NSCoding{
     
     //Decode each individual object and then create a new object instance
     required convenience init?(coder decoder: NSCoder) {
-        guard let name = decoder.decodeObjectForKey("name") as? String,
-            let startDate = decoder.decodeObjectForKey("startDate") as? NSDate,
-            let endDate = decoder.decodeObjectForKey("endDate") as? NSDate,
-            let automator = decoder.decodeObjectForKey("automator") as? Automator?
+        guard let name = decoder.decodeObject(forKey: "name") as? String,
+            let startDate = decoder.decodeObject(forKey: "startDate") as? Date,
+            let endDate = decoder.decodeObject(forKey: "endDate") as? Date,
+            let automator = decoder.decodeObject(forKey: "automator") as? Automator?
             else { return nil }
         
         self.init(aName: name, aStartDate: startDate, anEndDate: endDate)
         self.automator = automator
     }
     //Encoding fucntion for saving. Encode each object with a key for retervial
-    func encodeWithCoder(coder: NSCoder) {
-        coder.encodeObject(name, forKey: "name")
-        coder.encodeObject(startDate, forKey: "startDate")
-        coder.encodeObject(endDate, forKey: "endDate")
-        coder.encodeObject(automator, forKey: "automator")
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(startDate, forKey: "startDate")
+        coder.encode(endDate, forKey: "endDate")
+        coder.encode(automator, forKey: "automator")
     }
     
     //If two shows have the same name they are equal 
-    override func isEqual(object: AnyObject?) -> Bool {
+    override func isEqual(_ object: Any?) -> Bool {
         var result = false
-        if object!.isKindOfClass(Show) {
+        if (object! as AnyObject).isKind(of: Show.self) {
             if (object as! Show).name == name {
                 result = true
             }
@@ -53,11 +53,11 @@ class Show: NSObject, NSCoding{
     }
     
     override var description: String {
-        let timeFormatter = NSDateFormatter();timeFormatter.dateFormat = "hh:mm a"
-        let dayFormatter = NSDateFormatter();dayFormatter.dateFormat = "EEEE"
+        let timeFormatter = DateFormatter();timeFormatter.dateFormat = "hh:mm a"
+        let dayFormatter = DateFormatter();dayFormatter.dateFormat = "EEEE"
         //Convert the shows dates into strings
-        let startDay = dayFormatter.stringFromDate(startDate!);let startTime = timeFormatter.stringFromDate(startDate!)
-        let endDay = dayFormatter.stringFromDate(endDate!);let endTime = timeFormatter.stringFromDate(endDate!)
+        let startDay = dayFormatter.string(from: startDate!);let startTime = timeFormatter.string(from: startDate!)
+        let endDay = dayFormatter.string(from: endDate!);let endTime = timeFormatter.string(from: endDate!)
         
         var automatorString = "No"
         if automator != nil {automatorString = "Yes"}
